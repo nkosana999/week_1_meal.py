d40c2fe69aedcd194405006052ae16099a8791c1
nkosana999 <pnnkosi17@gmail.com>
2025-08-15 23:03:50 -0700

    Updated meal.py with bug fix

--- meal.py
+++ meal.py
@@ -1,21 +1,24 @@
+import sys
 def main():
+
     time = input("What time is it? ")
-    return convert(time)
+    meal_time = convert(time)
+
+    if 7 <= meal_time <= 8:
+        print("breakfast time")
+    elif 12 <= meal_time <= 13:
+        print("lunch time")
+    elif 18 <= meal_time <= 19:
+        print("dinner time")
 
 
 
 def convert(time):
-    time = time.split(":")
-    x = int(time[0])
-
-    if  7 <= x <= 8:
-        print("breakfast time")
-    elif 12 <= x <= 13:
-        print("lunch time")
-    elif 18 <= x <= 19:
-        print("dinner time")
-    else:
-        None
+    try:
+        hours, minutes = time.split(":")
+        return float(hours) + float(minutes) / 60
+    except ValueError:
+        sys.exit
 
 
 if __name__ == "__main__":
